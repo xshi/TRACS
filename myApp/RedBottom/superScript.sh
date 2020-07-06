6e2ae255fb7e7dab3181ada20d2f53ce97356cf1
suyu <1203111345@qq.com>
2020-07-06 17:46:00 +0800

    config for W13

--- myApp/RedBottom/superScript.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-for i in `seq -w 1 4`
-do
-  touch `date +%Y%m%d`-$i.txt
-  ../MfgTRACSFit $i 2015-09-01_15-21-40_PIN_7859_1.8_4_W2_J6-1_Vbias100-500.txt.root Config.TRACS_10._1. >> `date +%Y%m%d`-$i.txt
-  rm file*
-  mv output.root output-$i.root
-done
